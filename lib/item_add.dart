e5c30b1378987bf0c6c686eba3a2e47de9a26cc2
tsuchida <yu41926t.tutida@icloud.com>
2024-12-19 20:42:57 +0900

    item_addページの理解

--- lib/item_add.dart
+++ lib/item_add.dart
@@ -1,6 +1,4 @@
 
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
 
@@ -13,41 +11,40 @@ class TodoAddPage extends StatefulWidget {
 }
 
 class TodoAddPageState extends State<TodoAddPage> {
-  // 入力されたテキストをデータとして持つ
-  String _text = '';
+  String _inputText = '';
 
-  // データを元に表示するWidget
   @override
   Widget build(BuildContext context) {
     // タブの選択状態を使って適切な処理を行う
-    switch (widget.tabIndex) {
-      case 0:
-        debugPrint("卓球");
-        // 卓球の処理
-        break;
-      case 1:
-        debugPrint("サッカー");
-        // サッカーの処理
-        break;
-      case 2:
-        debugPrint("テニス");
-        // テニスの処理
-        break;
-      default:
-        break;
-    }
+    // switch (widget.tabIndex) {
+    //   case 0:
+    //     debugPrint("卓球");
+    //     // 卓球の処理
+    //     break;
+    //   case 1:
+    //     debugPrint("サッカー");
+    //     // サッカーの処理
+    //     break;
+    //   case 2:
+    //     debugPrint("テニス");
+    //     // テニスの処理
+    //     break;
+    //   default:
+    //     break;
+    // }
     return Scaffold(
+
       appBar: AppBar(
         title: const Text('リスト追加'),
       ),
+
       body: Container(
-        // 余白を付ける
-        padding: const EdgeInsets.all(64), //上下左右の各方向に対して同じ値（ここでは64）の余白を作成する
+        padding: const EdgeInsets.all(64), // 余白をつける
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // 入力されたテキストを表示
-            Text(_text, style: const TextStyle(color: Colors.blue)),
+            Text(_inputText, style: const TextStyle(color: Colors.blue)),
             const SizedBox(height: 8),
             // テキスト入力
             TextField(
@@ -57,7 +54,7 @@ class TodoAddPageState extends State<TodoAddPage> {
                 // データが変更したことを知らせる（画面を更新する）
                 setState(() {
                   // データを変更
-                  _text = value;
+                  _inputText = value;
                 });
               },
             ),
@@ -72,11 +69,11 @@ class TodoAddPageState extends State<TodoAddPage> {
                     //DBに保存
                     'todos', // テーブル名
                     {
-                      'content': _text, // カラム名: 値
+                      'content': _inputText, // カラム名: 値
                       'created_at': DateTime.now()
                           .millisecondsSinceEpoch, //データベースに新しいレコードを挿入する際に、そのレコードが作成された日時を表すための情報を設定しています。
                     },
-                  ).then((value) => Navigator.of(context).pop(_text)); //.then
+                  ).then((value) => Navigator.of(context).pop(_inputText)); //.then
                 },
                 child:
                     const Text('リスト追加', style: TextStyle(color: Colors.white)),
